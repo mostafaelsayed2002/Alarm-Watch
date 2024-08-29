754d5d98cbd4a92da99a0e5a97639e409f2a63cf
Mostafa Elsayed <mostafa.elsayed.2002@gmail.com>
2024-08-29 16:30:50 +0300

    add: System Timer Driver

---
+++ Nucleo-F446RE/MCAL/SYSTICK/Src/SYSTICK_program.c
@@ -0,0 +1,40 @@
+/***********************************************************************/
+/* @file   SYSTICK_program.c
+ * @author Mostafa Elsayed
+ * @brief  This file contains the implementation for the SYSTICK driver
+ * @date   29/6/2024
+ */
+#include <stdint.h>
+#include "STM32F466xx.h"
+#include "SYSTICK_interface.h"
+#include "SYSTICK_private.h"
+
+void SYSTICK_voidDelayms(uint32_t timems)
+{
+    uint32_t Ftick = SYSTICK_AHB_CLOCK / 8;
+    STK->VAL = 0;
+    // AHB/8
+    STK->CTRL &= ~(1 << 2);
+    STK->LOAD = (timems / 1000.0) * Ftick;
+    STK->CTRL |= 1 << 0;
+
+    while (((STK->CTRL >> 16) & 1) == 0)
+        ;
+    STK->CTRL &= ~(1 << 0);
+    STK->VAL = 0;
+}
+void SYSTICK_voidDelayus(uint32_t timeus)
+{
+    uint32_t Ftick = SYSTICK_AHB_CLOCK / 8;
+
+    STK->VAL = 0;
+    // AHB/8
+    STK->CTRL &= ~(1 << 2);
+    STK->LOAD = (timeus / 1000000.0) * Ftick;
+    STK->CTRL |= 1 << 0;
+
+    while (((STK->CTRL >> 16) & 1) == 0)
+        ;
+    STK->CTRL &= ~(1 << 0);
+    STK->VAL = 0;
+}
