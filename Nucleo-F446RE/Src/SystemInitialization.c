d923f24a6ed96f55e8218a147336e1b1b2aa8382
Mostafa Elsayed <mostafa.elsayed.2002@gmail.com>
2024-08-30 17:02:34 +0300

    Implement the system initialization

---
+++ Nucleo-F446RE/Src/SystemInitialization.c
@@ -0,0 +1,74 @@
+#include "SystemInitialization.h"
+#include "GPIO_interface.h"
+#include "RCC_interface.h"
+#include "UART_interface.h"
+#include "I2C_interface.h"
+
+void SYS_Initialization()
+{
+    /************************** GPIO Initialization ******************/
+
+    RCC_voidAHB1EnablePeripheralClock(0); // GPIOA
+    RCC_voidAHB1EnablePeripheralClock(1); // GPIOB
+
+    GPIO_PinConfig_t TX = {
+        .alternateFunction = AF7,
+        .mode = ALTERNATE_FUNCTION,
+        .outputSpeed = HIGH_SPEED,
+        .outputType = PUSH_PULL,
+        .pin = PIN2,
+        .port = PORTA,
+        .pullType = NO_PULL};
+    GPIO_PinConfig_t RX = {
+        .alternateFunction = AF7,
+        .mode = ALTERNATE_FUNCTION,
+        .outputSpeed = HIGH_SPEED,
+        .outputType = PUSH_PULL,
+        .pin = PIN3,
+        .port = PORTA,
+        .pullType = NO_PULL};
+    GPIO_PinConfig_t SCL = {
+        .alternateFunction = AF4,
+        .mode = ALTERNATE_FUNCTION,
+        .outputSpeed = LOW_SPEED,
+        .outputType = OPEN_DRAIN,
+        .pin = PIN8,
+        .port = PORTB,
+        .pullType = NO_PULL};
+    GPIO_PinConfig_t SDA = {
+        .alternateFunction = AF4,
+        .mode = ALTERNATE_FUNCTION,
+        .outputSpeed = LOW_SPEED,
+        .outputType = OPEN_DRAIN,
+        .pin = PIN9,
+        .port = PORTB,
+        .pullType = NO_PULL};
+
+    GPIO_u8InitPin(&TX);
+    GPIO_u8InitPin(&RX);
+    GPIO_u8InitPin(&SCL);
+    GPIO_u8InitPin(&SDA);
+
+    /************************** Peripherals Initialization ******************/
+
+    RCC_voidAPB1EnablePeripheralClock(17); // USART2
+    RCC_voidAPB1EnablePeripheralClock(21); // I2C1
+
+    UART_Config_t UART2_Config = {
+        .BaudRate = 9600,
+        .ClockFreq = 16000000,
+        .num = UART2,
+        .Over = UART_OVER16,
+        .Parity = UART_PARITY_DISABLED,
+        .Stop = UART_STOP_1BIT,
+        .WordLength = UART_8BIT};
+    I2C_Config_t I2C1_Config = {
+        .ClockSpeed = 0b01000,
+        .I2C = I2C1,
+        .Mode = I2C_SM,
+        .CCRValue = 0x28,
+        .TRISEValue = 0x09};
+
+    UART_u8Init(&UART2_Config);
+    I2C_Init(&I2C1_Config);
+}
